1cb38d6538440a2001eaf3e16fa4193140cc0cae
WashingtonCoutinho <contato@washingtoncoutinho.com.br>
2023-02-09 02:37:25 -0300

    New Sections Add

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,19 @@ module.exports = {
 
   theme: {
     extend: {
+      boxShadow: {
+        white: '0px 2px 0px 0px #FFFFFF',
+        primary: '3px 3px 0px 0px #EA7339',
+        secondary: '3px 3px 0px 0px #00A6CF',
+        third: '3px 3px 0px 0px #6D00B3',
+        wl: '3px 3px 0px 0px #FFFFFF',
+      },
       dropShadow: {
         white: '0px 2px 0px #FFFFFF',
-        primary: '0px 2px 0px #EA7339',
-        secondary: '0px 2px 0px #00A6CF',
-        third: '0px 2px 0px #6D00B3',
+        primary: '3px 3px 0px #EA7339',
+        secondary: '3px 3px 0px #00A6CF',
+        third: '3px 3px 0px #6D00B3',
+        wl: '3px 3px 0px #FFFFFF',
       },
       colors: {
         primary: '#EA7339',
